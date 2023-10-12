7a2a8d242ddc494803f5718027c9fcc893c3bbdc
interrrp <int@addictedto.coffee>
2023-10-12 18:02:12 +0800

    Set allowed mentions

--- eco/__main__.py
+++ eco/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from disnake import AllowedMentions
 from disnake.ext.commands import InteractionBot
 from loguru import logger
 
@@ -15,7 +16,9 @@ async def main() -> None:
     logger.info("Finished creating tables")
 
     logger.info("Starting bot")
-    bot = InteractionBot(test_guilds=settings.test_guild_ids)
+    bot = InteractionBot(
+        test_guilds=settings.test_guild_ids, allowed_mentions=AllowedMentions.none()
+    )
     bot.load_extensions("eco/exts")
     await bot.start(settings.token)
 
